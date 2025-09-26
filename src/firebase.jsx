fdb01f5f401000ab51dd8155e3624eee26490bfe
bernardowillis <bernardow03@gmail.com>
2025-09-25 20:32:44 -0600

    ahora sí final lab 6

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "por motivos de seguridad no puedo subir esto a github",
-  authDomain: "por motivos de seguridad no puedo subir esto a github",
-  projectId: "por motivos de seguridad no puedo subir esto a github",
-  storageBucket: "por motivos de seguridad no puedo subir esto a github",
-  messagingSenderId: "por motivos de seguridad no puedo subir esto a github",
-  appId: "por motivos de seguridad no puedo subir esto a github"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
